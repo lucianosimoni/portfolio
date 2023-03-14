7284ede562564db4feff10d73340244d3c2dca2e
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-03-14 18:31:06 +0000

    Background clipping working
    Started working in the figma model built yesterday - happy to start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import "./index.css";
-
 export default function App() {
+  // clip-path-inset-[0_36%_0_39%]
   return (
-    <div class="bg-slate-800 h-screen text-center">
-      <h1 className="text-xs text-white">Ola</h1>
-    </div>
+    <main className="bg-palette-white h-screen overflow-hidden">
+      <div
+        id="background-doodles"
+        className="fixed h-screen w-screen bg-doodle-pattern opacity-[3%] bg-cover bg-repeat "
+      ></div>
+
+      <div className="h-[calc(100vh+2px)] clip-path-inset-[0_40%_0_40%] transition-all active:clip-path-inset-[0_20%_0_20%] bg-mountain-dark  bg-cover"></div>
+    </main>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    backgroundImage: {
+      "doodle-pattern": "url('/src/assets/doodle-pattern.png')",
+      "mountain-dark": "url('/src/assets/mountain-dark.png')",
+    },
+    extend: {
+      colors: {
+        "palette-white": "#f7ede2",
+        "palette-green": "#84a59d",
+        "palette-pink": "#f5cac3",
+        "palette-yellow": "#f6bd60",
+        "palette-black": "#14213d",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwind-clip-path")],
 };
