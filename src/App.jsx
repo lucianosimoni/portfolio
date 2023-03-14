f9324b022d57cb0aa03acf6bebcbe03c2b9e8f80
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-03-14 18:41:57 +0000

    clip-path stable to continue

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 export default function App() {
-  // clip-path-inset-[0_36%_0_39%]
+  // clip-path-inset-
+  // middle [0_40%_0_40%]
+  // right [0_15%_0_65%]
+  // closed [0_50%_0_50%]
+
   return (
-    <main className="bg-palette-white h-screen overflow-hidden">
+    <main className="bg-palette-white h-screen w-screen">
       <div
         id="background-doodles"
         className="fixed h-screen w-screen bg-doodle-pattern opacity-[3%] bg-cover bg-repeat "
       ></div>
 
-      <div className="h-[calc(100vh+2px)] clip-path-inset-[0_40%_0_40%] transition-all active:clip-path-inset-[0_20%_0_20%] bg-mountain-dark  bg-cover"></div>
+      {/* FIXME: h-full is with a 1px gap in the bottom - Possible solution: h-[calc(100vh+2px)] and overflow-hidden in its parent*/}
+      <div className="bg-mountain-dark bg-cover h-full clip-path-inset-[0_40%_0_40%] transition-[clip-path] "></div>
     </main>
   );
 }
