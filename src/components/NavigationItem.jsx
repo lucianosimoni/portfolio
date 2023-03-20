d664d8479c48dc2b250bf6bfbe36e9a9029d9de4
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-03-20 23:40:58 +0000

    Issue fixed for good
    The import variables were not being built due to it not being used by the code - I defined this as the bug

--- src/components/NavigationItem.jsx
+++ src/components/NavigationItem.jsx
@@ -8,6 +8,19 @@ import contactsSvg from "/src/assets/contacts.svg";
 export default function NavigationItem({ onClick, name }) {
   // FIXME: Icons are just black, not nice.
   // TODO: Add the current section and update navigation with colored yellow dot.
+
+  const formattedName = name.replace(/ /g, "").toLowerCase();
+  const formattedPath = `/src/assets/${formattedName}.svg`;
+  const svgs = [
+    homeSvg,
+    projectsSvg,
+    aboutmeSvg,
+    askmyaiSvg,
+    educationSvg,
+    contactsSvg,
+  ];
+  const icon = svgs[svgs.indexOf(formattedPath)];
+
   return (
     <li
       onClick={onClick}
@@ -15,7 +28,7 @@ export default function NavigationItem({ onClick, name }) {
     >
       <img
         className="absolute group-hover/nav:opacity-100 group-hover/nav:scale-100 scale-0 opacity-0 left-[3.5px] top-[3.5px] transition-all duration-500"
-        src={eval(`${name.replace(/ /g, "").toLowerCase()}Svg`)}
+        src={icon}
       />
       <div className="group-hover/nav:scale-0 border-4 rounded-full border-palette-black bg-palette-black p-2 duration-500 transition-all"></div>
       <span className="absolute left-[-170%] top-[16px] font-bold w-[6rem] text-right opacity-0 group-hover/li:opacity-100 transition-[opacity] duration-300">
